cb92661b629644702e05f142bb908bdcc5b5e11b
Vinicius Bastos <vclemente2@hotmail.com>
2023-08-26 14:29:28 -0300

    Prática da sintaxe de classes, métodos e atributos.

--- tipos-variaveis/src/SmartTv.java
+++ tipos-variaveis/src/SmartTv.java
@@ -11,6 +11,14 @@ public class SmartTv {
         tv.increaseVolume();
         tv.increaseVolume();
         tv.toggleOnOff();
+        tv.reduceVolume();
+        tv.toggleOnOff();
+        tv.reduceVolume();
+        tv.changeChannel(10);
+        tv.increaseChannel();
+        tv.reduceChannel();
+        tv.reduceChannel();
+        tv.reduceChannel();
 
     }
 
@@ -22,14 +30,48 @@ public class SmartTv {
         }
     }
 
+    public void reduceVolume() {
+        if (this.on) {
+            this.printVolume(--this.volume);
+        } else {
+            System.out.println("The tv must be on to change the volume.");
+        }
+    }
+
+    public void changeChannel(int channel) {
+        this.channel = channel;
+        this.printChannel(channel);
+    }
+
+    public void increaseChannel() {
+        if (this.on) {
+            this.changeChannel(++this.channel);
+        } else {
+            System.out.println("The tv must be on to change the channel.");
+        }
+    }
+
+    public void reduceChannel() {
+        if (this.on) {
+            this.changeChannel(--this.channel);
+        } else {
+            System.out.println("The tv must be on to change the channel.");
+        }
+    }
+
     public void toggleOnOff() {
         this.on = !this.on;
         String tvStatus = this.on ? "Tv on" : "Tv off";
+
         System.out.println(tvStatus);
     }
 
     private void printVolume(int volume) {
         System.out.println("Volume: " + volume);
+    }
+
+    private void printChannel(int channel) {
+        System.out.println("Channel: " + channel);
     }
 
 }
